6c197aeec0536029ffd1160299f650eea97aebdc
SergioComeron <sergiocomeron@icloud.com>
2025-08-17 17:19:26 +0200

    Initial Commit

---
+++ TrackGym/TrackGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackGymApp.swift
+//  TrackGym
+//
+//  Created by Sergio Comerón on 17/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
